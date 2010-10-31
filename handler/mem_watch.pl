eae56c12e36bb539593782b96bb011254d4825a5
Christian Walde <walde.christian@googlemail.com>
2010-10-31 02:49:25 +0200

    moved stack function to html so opera won't complain

--- handler/mem_watch.pl
+++ handler/mem_watch.pl
@@ -61,7 +61,7 @@ sub make_graph {
     my $json = to_json( $rows );
 
     return qq|
-        <h3>$data->{program} ($count)</h3>
+        <h3>$data->{program} ($count requests)</h3>
         <div class="fig">
             <script type="text/javascript+protovis">
                 var data = $json;
@@ -82,11 +82,61 @@ sub page {
 <head>
     <title>Memory Progression Charts</title>
     <script type="text/javascript" src="protovis-r3.2.js"></script>
-    <script type="text/javascript" src="stack.js"></script>
-    <style type="text/css">
 
+    <script type="text/javascript">
+        function stack( data, start, end, min, max ) {
 
-    </style>
+            var w = 400,
+            h = 80,
+            x = pv.Scale.
+                linear(start, end).
+                range(0, w),
+            y = pv.Scale.
+                linear(min, max).
+                range(0, h);
+
+            /* The root panel. */
+            var vis = new pv.Panel()
+                .width(w)
+                .height(h)
+                .bottom(20)
+                .left(20)
+                .right(10)
+                .top(5);
+
+            /* X-axis ticks. */
+            vis.add(pv.Rule)
+                .data(x.ticks())
+                .visible(function(d) d > 0)
+                .left(x)
+                .strokeStyle("#eee")
+              .add(pv.Rule)
+                .bottom(-5)
+                .height(5)
+                .strokeStyle("#000")
+              .anchor("bottom").add(pv.Label)
+                .text(x.tickFormat);
+
+            /* Y-axis ticks. */
+            vis.add(pv.Rule)
+                .data(y.ticks(5))
+                .bottom(y)
+                .strokeStyle(function(d) d ? "#eee" : "#000")
+              .anchor("left").add(pv.Label)
+                .text(y.tickFormat);
+
+            /* The line. */
+            vis.add(pv.Line)
+                .data(data)
+                .interpolate("step-after")
+                .left(function(d) x(d.x))
+                .bottom(function(d) y(d.y))
+                .lineWidth(3);
+
+            vis.render();
+        }
+    </script>
+
 </head>
 
 <body>
